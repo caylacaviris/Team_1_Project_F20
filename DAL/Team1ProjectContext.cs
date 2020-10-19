524fe56b3e142b9e0551897229d7b9a2a519b1e0
Cayla Caviris <cc465416@ohio.edu>
2020-10-19 10:16:40 -0400

    DAL DbContext and connection string

--- DAL/Team1ProjectContext.cs
+++ DAL/Team1ProjectContext.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Team_1_Project.Models;
 
 namespace Team_1_Project.DAL
 {
-    public class Team1ProjectContext
+    public class Team1ProjectContext: DbContext
     {
+        public Team1ProjectContext() : base("name = DefaultConnection")
+        {
+        }
+            public DbSet<userData> userData { get; set; }
+         
+   
     }
 }
