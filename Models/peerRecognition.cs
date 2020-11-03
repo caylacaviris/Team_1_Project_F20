c7ba6108e9271d6d7c74057ad32dfdb0ca81b15c
Cayla Caviris <cc465416@ohio.edu>
2020-11-03 12:38:37 -0500

    peer recognition started

---
+++ Models/peerRecognition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Team_1_Project.Models
+{
+    public class peerRecognition
+    {
+        public int ID { get; set; }
+       
+        [Display(Name = "ID of the Person giving the recognition")]
+        public Guid recognizor { get; set; }
+
+        [Display(Name = "ID of the Person receiving the recognition")]
+        public Guid recognized { get; set; }
+
+        [Display(Name ="Date recognition given")]
+        public DateTime recognizationDate { get; set; }
+    }
+}
